642a9d535739812b248239677c135ba124f03516
Hans-Christian Otto <c.otto@suora.com>
2024-10-07 12:22:05 +0200

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -7,15 +7,4 @@ if (container === null) {
   const element = <h1>Hallo Erfurt!!</h1>;
   const reactRoot = createRoot(container);
   reactRoot.render(element);
-
-  // Hiermit würde die Anwendung nach 3 Sekunden wieder verschwinden
-  // setTimeout(() => {
-  //   reactRoot.unmount();
-  // }, 3_000_000);
 }
-
-// Die React-Doku schlägt folgende Lösung mit dem Ausrufezeichen vor.
-// Diese unterdrückt aber potentiell Probleme, deswegen rate ich zu der oberen Variante
-// const element = <h1>Hallo Erfurt!!</h1>;
-// const reactRoot = createRoot(container!);
-// reactRoot.render(element);

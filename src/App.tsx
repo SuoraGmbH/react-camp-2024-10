81fc53ffcaef9c64d5bbd5a7b2d368b7f4c0a250
Hans-Christian Otto <c.otto@suora.com>
2024-10-07 12:23:04 +0200

    add app component

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <div>Wann ist Mittagspause?</div>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
 
 const container = document.getElementById("root");
 if (container === null) {
   alert("Es gibt kein Element mit der id root");
 } else {
-  const element = <h1>Hallo Erfurt!!</h1>;
   const reactRoot = createRoot(container);
-  reactRoot.render(element);
+  reactRoot.render(<App />);
 }

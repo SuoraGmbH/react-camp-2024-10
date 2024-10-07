d1994bf4ff214caf6de33c7b0881c236e0f8b5a0
Hans-Christian Otto <c.otto@suora.com>
2024-10-07 11:52:16 +0200

    Show React 18 syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,21 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-const element = <h1>Hallo Erfurt!!</h1>;
+const container = document.getElementById("root");
+if (container === null) {
+  alert("Es gibt kein Element mit der id root");
+} else {
+  const element = <h1>Hallo Erfurt!!</h1>;
+  const reactRoot = createRoot(container);
+  reactRoot.render(element);
 
-ReactDOM.render(element, document.getElementById("root"));
+  // Hiermit würde die Anwendung nach 3 Sekunden wieder verschwinden
+  // setTimeout(() => {
+  //   reactRoot.unmount();
+  // }, 3_000_000);
+}
+
+// Die React-Doku schlägt folgende Lösung mit dem Ausrufezeichen vor.
+// Diese unterdrückt aber potentiell Probleme, deswegen rate ich zu der oberen Variante
+// const element = <h1>Hallo Erfurt!!</h1>;
+// const reactRoot = createRoot(container!);
+// reactRoot.render(element);

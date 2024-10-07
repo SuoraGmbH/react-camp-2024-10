0ce3d7233f904075d5e5165fc72bd81f38be330a
Hans-Christian Otto <c.otto@suora.com>
2024-10-07 14:15:25 +0200

    React Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Welcome } from "./components/Welcome.tsx";
 
 const App: React.FunctionComponent = () => {
   return (
-    <div>
+    <>
       <Welcome />
       <Welcome city="Oer-Erkenschwick" />
       Wann ist Mittagspause?
-    </div>
+    </>
   );
 };
 

920e89b6a83b5d27eb9d825a5fdd24f6f3af2631
Hans-Christian Otto <c.otto@suora.com>
2024-10-07 16:44:47 +0200

    showcase controlled component

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TimeEntryForm: React.FunctionComponent = () => {
+  const [inputValue, setInputValue] = useState("");
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setInputValue(event.target.value);
+  };
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
     console.log("Hallo Welt!");
+    setInputValue("");
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <div>
+        <input onChange={handleChange} value={inputValue} />
+      </div>
+      <p>{inputValue}</p>
       <button type="submit">Speichern</button>
     </form>
   );

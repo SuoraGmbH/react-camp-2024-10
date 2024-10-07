6e7202631f239096d0d011959a55fd21a944a9c3
Hans-Christian Otto <c.otto@suora.com>
2024-10-07 13:53:28 +0200

    Show fat arrow function & typings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-function App() {
+import * as React from "react";
+
+const App: React.FunctionComponent = () => {
+  return <div>Wann ist Mittagspause?</div>;
+};
+
+export function App2(): React.ReactNode {
   return <div>Wann ist Mittagspause?</div>;
 }
 

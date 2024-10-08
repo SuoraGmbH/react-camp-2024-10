6b4793a9f2db16af1cb07333e1383a31e0d8f40a
Hans-Christian Otto <c.otto@suora.com>
2024-10-08 13:42:36 +0200

    move timeentries to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import * as React from "react";
 import TimeEntryForm from "./components/TimeEntryForm.tsx";
 import TimeEntryList from "./components/TimeEntryList.tsx";
+import TimeEntry from "./domain/TimeEntry.ts";
 
 const App: React.FunctionComponent = () => {
+  const timeEntries: TimeEntry[] = [
+    {
+      id: "ecf06450-2cf7-4ce9-adf6-7263e3027422",
+      comment: "React lernen",
+      start: new Date(),
+      end: new Date(),
+    },
+    {
+      id: "ecf06450-2cf7-4ce9-adf8-7263e3027422",
+      comment: "React testing lernen",
+      start: new Date(),
+      end: new Date(),
+    },
+    {
+      id: "ecf06450-2cf7-3ce9-adf6-7263e3027422",
+      comment: "React Query lernen",
+      start: new Date(),
+      end: new Date(),
+    },
+  ];
+
   return (
     <>
-      <TimeEntryList />
+      <TimeEntryList timeEntries={timeEntries} />
       <TimeEntryForm onNewTimeEntry={(timeEntry) => console.log(timeEntry)} />
     </>
   );
--- src/components/TimeEntryList.tsx
+++ src/components/TimeEntryList.tsx
@@ -2,28 +2,11 @@ import React from "react";
 import TimeEntry from "../domain/TimeEntry.ts";
 import TimeEntryView from "./TimeEntryView.tsx";
 
-const TimeEntryList: React.FunctionComponent = () => {
-  const timeEntries: TimeEntry[] = [
-    {
-      id: "ecf06450-2cf7-4ce9-adf6-7263e3027422",
-      comment: "React lernen",
-      start: new Date(),
-      end: new Date(),
-    },
-    {
-      id: "ecf06450-2cf7-4ce9-adf8-7263e3027422",
-      comment: "React testing lernen",
-      start: new Date(),
-      end: new Date(),
-    },
-    {
-      id: "ecf06450-2cf7-3ce9-adf6-7263e3027422",
-      comment: "React Query lernen",
-      start: new Date(),
-      end: new Date(),
-    },
-  ];
+interface Props {
+  timeEntries: TimeEntry[];
+}
 
+const TimeEntryList: React.FunctionComponent<Props> = ({ timeEntries }) => {
   return (
     <>
       {timeEntries.map((timeEntry) => (

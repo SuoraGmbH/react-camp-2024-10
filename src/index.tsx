0dd4d478bc6aaab77401228b03bcfe7537ea7a62
Hans-Christian Otto <c.otto@suora.com>
2024-10-08 09:08:25 +0200

    First example with fetch

--- src/index.tsx
+++ src/index.tsx
@@ -8,3 +8,25 @@ if (container === null) {
   const reactRoot = createRoot(container);
   reactRoot.render(<App />);
 }
+
+const fetchReactData = () => {
+  console.log("ich lade daten");
+  const responsePromise = fetch("https://api.github.com/repos/facebook/react");
+  console.log(responsePromise);
+
+  responsePromise.then(function (response) {
+    console.log(response);
+  });
+
+  responsePromise.catch(function (reason) {
+    console.warn("Hier kommt meine Meldung");
+    console.error(reason);
+    console.warn("Das war meine Meldung");
+  });
+
+  responsePromise.finally(function () {
+    console.log("Wir sind finally fertig!");
+  });
+};
+
+fetchReactData();

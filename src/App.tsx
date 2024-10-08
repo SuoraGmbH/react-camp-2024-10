445c22200f4670a7ad5736ee6f941499b9e76171
Hans-Christian Otto <c.otto@suora.com>
2024-10-08 13:56:30 +0200

    Initialize with empty array & generic type for state

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import * as React from "react";
 import TimeEntryForm from "./components/TimeEntryForm.tsx";
 import TimeEntryList from "./components/TimeEntryList.tsx";
-import TimeEntry from "./domain/TimeEntry.ts";
 import { useState } from "react";
+import TimeEntry from "./domain/TimeEntry.ts";
 
 const App: React.FunctionComponent = () => {
-  const initialTimeEntries: TimeEntry[] = [
-    {
-      id: "ecf06450-2cf7-4ce9-adf6-7263e3027422",
-      comment: "React lernen",
-      start: new Date(),
-      end: new Date(),
-    },
-    {
-      id: "ecf06450-2cf7-4ce9-adf8-7263e3027422",
-      comment: "React testing lernen",
-      start: new Date(),
-      end: new Date(),
-    },
-    {
-      id: "ecf06450-2cf7-3ce9-adf6-7263e3027422",
-      comment: "React Query lernen",
-      start: new Date(),
-      end: new Date(),
-    },
-  ];
-
-  const [timeEntries, setTimeEntries] = useState(initialTimeEntries);
+  const [timeEntries, setTimeEntries] = useState<TimeEntry[]>([]);
 
   return (
     <>

9f70679a7f7fb2c9ed7b070320cf199572b31741
Hans-Christian Otto <c.otto@suora.com>
2024-10-08 14:49:29 +0200

    more tanstack query features

--- src/components/GithubRepoStats.tsx
+++ src/components/GithubRepoStats.tsx
@@ -9,22 +9,29 @@ interface Props {
 export const GithubRepoStats: React.FunctionComponent<Props> = ({
   repoName,
 }) => {
-  const { data, isLoading, isSuccess } = useQuery({
-    queryFn: () => fetchGithubRepoData(repoName),
-    queryKey: [repoName],
-  });
+  const { data, isFetching, isLoading, isSuccess, refetch, isStale, error } =
+    useQuery({
+      queryFn: () => fetchGithubRepoData(repoName),
+      queryKey: [repoName],
+    });
 
   if (isLoading) {
     return <div>Loading…</div>;
   }
 
+  if (error) {
+    return <div>kaputt: {error.message}</div>;
+  }
+
   if (!isSuccess) {
-    return <div>kaputt</div>;
+    return <div>kaputt und ich weiß nicht wieso.</div>;
   }
 
   return (
-    <div>
+    <div style={{ opacity: isFetching && isStale ? 0.2 : 1 }}>
+      <button onClick={() => refetch()}>♻️</button>
       {repoName} hat {data.stargazers_count.toLocaleString()} Sternchen ⭐
+      {isFetching ? "♻️" : ""}
     </div>
   );
 };

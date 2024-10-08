3fe7be8c3e0918f5bead9740aee5c9469e4714e2
Hans-Christian Otto <c.otto@suora.com>
2024-10-08 10:52:08 +0200

    cleanup

--- src/components/GithubRepoStats.tsx
+++ src/components/GithubRepoStats.tsx
@@ -3,12 +3,10 @@ import { fetchGithubRepoData } from "../fetchGithubRepoData.ts";
 
 interface Props {
   repoName: string;
-  highlight?: boolean;
 }
 
 export const GithubRepoStats: React.FunctionComponent<Props> = ({
   repoName,
-  highlight = false,
 }) => {
   const [stargazersCount, setStargazersCount] = useState(0);
 
@@ -23,11 +21,7 @@ export const GithubRepoStats: React.FunctionComponent<Props> = ({
   }
 
   return (
-    <div
-      style={{
-        border: highlight ? "2px solid pink" : "",
-      }}
-    >
+    <div>
       {repoName} hat {stargazersCount} Sternchen ⭐
     </div>
   );

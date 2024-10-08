828a2098efa81442880474ea7ffb8d819d9c261a
Hans-Christian Otto <c.otto@suora.com>
2024-10-08 11:46:07 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
 import * as React from "react";
-import { Welcome } from "./components/Welcome.tsx";
-import TimeEntryView from "./components/TimeEntryView.tsx";
-import TimeEntry from "./domain/TimeEntry.ts";
 import TimeEntryForm from "./components/TimeEntryForm.tsx";
-import Counter from "./components/Counter.tsx";
-import { GithubRepoStats } from "./components/GithubRepoStats.tsx";
-import DynamicGithubRepoStats from "./components/DynamicGithubRepoStats.tsx";
 import TimeEntryList from "./components/TimeEntryList.tsx";
 
-function getTimeEntryFromBackend() {
-  const timeEntry: TimeEntry = {
-    id: "4711",
-    comment: "React Workshop",
-    start: new Date(),
-    end: new Date(),
-  };
-
-  return timeEntry;
-}
-
 const App: React.FunctionComponent = () => {
-  const timeEntry = getTimeEntryFromBackend();
   return (
     <>
       <TimeEntryList />
-      <DynamicGithubRepoStats key={23} />
-      <GithubRepoStats repoName="facebook/react" />
-      <Counter />
       <TimeEntryForm />
-      <Welcome />
-      <Welcome city="Oer-Erkenschwick" />
-      <TimeEntryView timeEntry={timeEntry} />
-      Wann ist Mittagspause?
     </>
   );
 };

fd1c802ea7c34fbb6945e5597e0eb4263cc73fed
Hans-Christian Otto <c.otto@suora.com>
2024-10-08 10:30:11 +0200

    cleanup

--- src/components/GithubRepoStats.tsx
+++ src/components/GithubRepoStats.tsx
@@ -3,29 +3,16 @@ import { fetchReactRepoData } from "../fetchReactRepoData.ts";
 
 export const GithubRepoStats: React.FunctionComponent = () => {
   const [stargazersCount, setStargazersCount] = useState(0);
-  const [reloadCounter, setReloadCounter] = useState(0);
-  console.log("GithubRepoStats rendert grade");
 
   useEffect(() => {
-    console.log("Effekt wird ausgeführt");
     fetchReactRepoData().then((repoData) => {
-      console.log("Daten sind da");
       setStargazersCount(repoData.stargazers_count);
     });
-  }, [reloadCounter]);
+  }, []);
 
   if (stargazersCount === 0) {
-    console.log("GithubRepoStats ist fertig mit dem rendern");
     return <div>Loading…</div>;
   }
 
-  console.log("GithubRepoStats ist fertig mit dem rendern");
-  return (
-    <div>
-      <button onClick={() => setReloadCounter((counter) => counter + 1)}>
-        Neuladen (Counter: {reloadCounter})
-      </button>
-      facebook/react hat {stargazersCount} Sternchen ⭐
-    </div>
-  );
+  return <div>facebook/react hat {stargazersCount} Sternchen ⭐</div>;
 };

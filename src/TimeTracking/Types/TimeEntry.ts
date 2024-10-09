22c767d63e4e890bbbb66afbd29f03d1670c0c80
Hans-Christian Otto <c.otto@suora.com>
2024-10-09 09:37:03 +0200

    Use zod instead of handwritten type

--- src/TimeTracking/Types/TimeEntry.ts
+++ src/TimeTracking/Types/TimeEntry.ts
@@ -1,15 +1,11 @@
 import { z } from "zod";
 
-export default interface TimeEntry {
-  id: string;
-  comment: string;
-  start: Date;
-  end: Date;
-}
-
 export const timeEntrySchema = z.object({
   id: z.string(),
   comment: z.string(),
   start: z.coerce.date(),
   end: z.coerce.date(),
 });
+
+type TimeEntry = z.infer<typeof timeEntrySchema>;
+export default TimeEntry;

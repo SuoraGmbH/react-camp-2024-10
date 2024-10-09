5e25f7bbcaaa5711befa44c8a0369944b28ec551
Hans-Christian Otto <c.otto@suora.com>
2024-10-09 10:53:26 +0200

    Showcase useCounter hook

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
-
-const Counter: React.FunctionComponent = () => {
+const useCounter = () => {
   const [counter, setCounter] = useState(0);
 
-  const handleClick = () => {
-    setCounter((counter) => counter + 1);
+  // return [counter, () => setCounter((counter) => counter + 1)];
+  return {
+    counter,
+    increment: () => setCounter((counter) => counter + 1),
+    decrement: () => setCounter((counter) => counter - 1),
   };
+};
+
+const Counter: React.FunctionComponent = () => {
+  // const [counter, increment] = useCounter();
+  const { counter, increment } = useCounter();
 
   return (
     <div>
-      Counter: {counter} <button onClick={handleClick}>Increment</button>
+      Counter: {counter} <button onClick={increment}>Increment</button>
     </div>
   );
 };

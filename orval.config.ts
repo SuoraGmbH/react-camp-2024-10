59a261e42f4fa9f4859648d21ebe66b1d6fa0c08
Hans-Christian Otto <c.otto@suora.com>
2024-10-09 14:55:25 +0200

    add msw mocks to orval

--- orval.config.ts
+++ orval.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       target: "src/TimeTracking/Api/Client",
       schemas: "src/TimeTracking/Api/Schema",
       client: "react-query",
+      mock: true,
     },
   },
   jsonServerZod: {

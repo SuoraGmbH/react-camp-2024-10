4c92fd1b7302c580d287a89af6bdce9edd988f3d
Hans-Christian Otto <c.otto@suora.com>
2024-10-09 15:23:31 +0200

    Create custom render function

--- src/Github/GithubRepoStats.test.tsx
+++ src/Github/GithubRepoStats.test.tsx
@@ -1,49 +1,40 @@
-import { render, screen } from "@testing-library/react";
 import { GithubRepoStats } from "./GithubRepoStats.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { beforeEach } from "vitest";
 import { expect } from "@storybook/test";
 import { mockServer } from "../setupTests.ts";
 import { githubRepoFacebookReactSuccessMockHandler } from "./mocks/repo/facebook/react/success.ts";
 import { githubRepoFacebookReactRateLimitedMockHandler } from "./mocks/repo/facebook/react/rateLimitted.ts";
+import { render, screen } from "../test-utils.tsx";
 
 describe("<GithubRepoStats />", () => {
-  beforeEach(() => {
-    mockServer.use(githubRepoFacebookReactSuccessMockHandler);
-    const queryClient = new QueryClient();
-    render(
-      <QueryClientProvider client={queryClient}>
-        <GithubRepoStats repoName="facebook/react" />
-      </QueryClientProvider>,
-    );
+  describe("Successful Requests", () => {
+    beforeEach(() => {
+      mockServer.use(githubRepoFacebookReactSuccessMockHandler);
+    });
+
+    test("renders without crashing", () => {
+      render(<GithubRepoStats repoName="facebook/react" />);
+    });
+
+    it("renders the loading indicator", () => {
+      render(<GithubRepoStats repoName="facebook/react" />);
+      expect(screen.getByText(/loading/i)).toBeVisible();
+    });
+
+    it("renders the data from the server", async () => {
+      render(<GithubRepoStats repoName="facebook/react" />);
+      await screen.findByText(/facebook\/react/);
+      expect(screen.getByText("4.711", { exact: false })).toBeVisible();
+    });
   });
 
-  test("renders without crashing", () => {});
-  it("renders the loading indicator", () => {
-    expect(screen.getByText(/loading/i)).toBeVisible();
-  });
+  test("renders an error if we were rate limitted", async () => {
+    mockServer.use(githubRepoFacebookReactRateLimitedMockHandler);
+    render(<GithubRepoStats repoName="facebook/react" />);
 
-  it("renders the data from the server", async () => {
-    await screen.findByText(/facebook\/react/);
-    expect(screen.getByText("4.711", { exact: false })).toBeVisible();
+    await screen.findByText(/kaputt/i);
+    expect(
+      screen.getByText(/HTTP request failed with status 403/),
+    ).toBeVisible();
   });
-});
-
-test("renders an error if we were rate limitted", async () => {
-  mockServer.use(githubRepoFacebookReactRateLimitedMockHandler);
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-      },
-    },
-  });
-  render(
-    <QueryClientProvider client={queryClient}>
-      <GithubRepoStats repoName="facebook/react" />
-    </QueryClientProvider>,
-  );
-
-  await screen.findByText(/kaputt/i);
-  expect(screen.getByText(/HTTP request failed with status 403/)).toBeVisible();
 });

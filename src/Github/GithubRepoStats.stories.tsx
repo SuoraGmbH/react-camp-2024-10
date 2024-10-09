46b781c3825e4fff45079c6ee5555e94978e246b
Hans-Christian Otto <c.otto@suora.com>
2024-10-09 15:35:54 +0200

    Multiple GithubRepoStats stories

--- src/Github/GithubRepoStats.stories.tsx
+++ src/Github/GithubRepoStats.stories.tsx
@@ -23,7 +23,12 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const RealRequest: Story = {
+  args: {
+    repoName: "facebook/react",
+  },
+};
+export const MockedSuccessResponse: Story = {
   args: {
     repoName: "facebook/react",
   },
